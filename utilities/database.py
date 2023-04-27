dfb2a1fe531c26f7902a7ffe125bb19dfdb22667
= <=>
2023-04-27 11:09:31 -0500

    updated references from config to Streamlit secrets

--- utilities/database.py
+++ utilities/database.py
@@ -1,10 +1,9 @@
 import deta
 import streamlit as st
 import streamlit_authenticator as stauth
-from config import DETA_KOALAFOUNDERS_DB_KEY
+# from config import DETA_KOALAFOUNDERS_DB_KEY
 
-
-deta = deta.Deta(DETA_KOALAFOUNDERS_DB_KEY)
+deta = deta.Deta(st.secrets['DETA_KOALAFOUNDERS_DB_KEY'])
 db = deta.Base('Users')
 
 
